312fafa95cd3b318efae53953ec9d6881b713db0
Big2XG-DEV <73581232+Big2XG-DEV@users.noreply.github.com>
2021-03-21 11:59:37 +0200

    Update firebase.js

--- src/features/firebase.js
+++ src/features/firebase.js
@@ -1,13 +1,7 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBSj3vvtIgXJc3SSM_uGtWjHO_q9_InwH0",
-  authDomain: "discordclone-f7337.firebaseapp.com",
-  projectId: "discordclone-f7337",
-  storageBucket: "discordclone-f7337.appspot.com",
-  messagingSenderId: "167105530012",
-  appId: "1:167105530012:web:5825df264dc014c47e0559",
-  measurementId: "G-2H8VT00SW7",
+"PASTE YOUR FIREBASE CONFIG HERE"
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
